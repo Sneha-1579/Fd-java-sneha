253483d55b4885c79906c25e2ddcc4d735706900
Sneha Tripathi <snehatripathi1002@gmail.com>
2025-08-11 20:00:04 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String args[]){
+        String movie = "Avengers";
+        int $bills = 9999999;
+        String _actor = "Robert Doweney Jr";
+        System.out.println(movie );
+        System.out.println($bills );
+        System.out.println(_actor );
+
+    }
+}

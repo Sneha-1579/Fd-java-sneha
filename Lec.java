9972160e892c05bf857b62b6f01af3daff3c7fbd
Sneha Tripathi <snehatripathi1002@gmail.com>
2025-08-11 20:00:47 +0530

    Add files via upload

---
+++ Lec.java
@@ -0,0 +1,14 @@
+public class Lec {
+    public static void main(String[] args[])
+    {
+        int a = 9001;
+        int b = 4567;
+        int sum =  a + b;
+        System.out.println(sum);
+
+        
+    }
+
+    
+    
+}

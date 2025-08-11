4fa25b0a3a881a4f6626d38bb6d7b1c5d78ae20c
Sneha Tripathi <snehatripathi1002@gmail.com>
2025-08-11 20:01:20 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,22 @@
+public class Datatype{
+    public static void main(String[] args) {
+        int a = 25;
+        float b = 3.14f;
+        double c = 25.56;
+        char d = 'A';
+        boolean e = true;
+        byte f = 100;
+        short g = 32000;
+        long h = 123456789L;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        
+    }
+
+}

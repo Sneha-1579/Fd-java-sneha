e584e5a31a764bc04b81e1dd971186537e55ee5c
Sneha Tripathi <snehatripathi1002@gmail.com>
2025-08-12 09:42:03 +0530

    Add files via upload

---
+++ Userinput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Userinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        // int a = sc.nextInt();
+        // System.out.println(a);
+        // // sum of two number
+        // System.out.println("enter two numbers");
+        // int num1 = sc.nextInt();
+        // int num2 = sc.nextInt();
+        // int sum = num1 + num2;
+        // System.out.println(sum);
+        
+        float x = sc.nextFloat();
+        System.out.println("sum of two numbers");
+        float y = sc.nextFloat();
+        float z = sc.nextFloat();
+        System.out.println(y+z);
+
+        
+    }
+    
+}

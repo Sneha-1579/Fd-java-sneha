f0067a5e23cbd427210b43ea6ed8cbae61068fe2
Sneha Tripathi <snehatripathi1002@gmail.com>
2025-08-21 14:04:49 +0530

    Add files via upload

---
+++ Lecture.java
@@ -0,0 +1,24 @@
+public class Lecture {
+    public static void main(String[] args) {
+        int a = 21;
+        int b= 10;
+        int sum = a + b;
+        int subtract = a - b;
+        int multiply = a * b;
+        int divide = a / b;
+        int modulus = a % b;
+        System.out.println(sum);
+        System.out.println(subtract);
+        System.out.println(multiply);
+        System.out.println(divide);
+        System.out.println(modulus);
+
+        
+
+
+
+
+
+
+      }
+}

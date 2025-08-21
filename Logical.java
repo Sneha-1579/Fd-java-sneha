14bb438efe2aa67e9adfead5d42436e293b2d753
Sneha Tripathi <snehatripathi1002@gmail.com>
2025-08-21 14:05:18 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,17 @@
+public class Logical {
+    public static void main(String[] args) {
+        // LOGICAL OPERATORS
+        int a = 10;
+        int b = 20;
+        System.out.println(a > 5 && b < 30);
+        System.out.println(a < 5 || b < 30);
+        System.out.println(!(a < 5));
+        
+       
+       
+
+
+
+    }
+    
+}
